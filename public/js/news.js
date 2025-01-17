22204731513404c48f7564560abd379710ec03d2
Albin Touma <albin.touma@gmail.com>
2025-01-17 11:16:20 +0000

    refractor

--- public/js/news.js
+++ public/js/news.js
@@ -2,6 +2,22 @@
 
 
 const news = [
+
+`
+LYON, France – INTERPOL has published its first-ever <a href="https://www.interpol.int/News-and-Events/News/2025/INTERPOL-publishes-first-Silver-Notice-targeting-criminal-assets"> Silver Notice </a> to help trace and recover criminal assets, combat transnational organized crime and enhance international police cooperation.
+
+The Notice, requested by Italy, seeks information on the assets belonging to a senior member of the mafia.
+`,
+
+`
+A global law enforcement operation headed by <a href="https://thehackernews.com/2024/12/interpol-arrests-5500-in-global.html"> Interpol </a> has led to the arrest of more than 5,500 suspects involved in financial crimes and the seizure of more than $400 million in virtual assets and government-backed currencies.
+
+
+`,
+
+`
+Egypt <a href="https://www.middleeasteye.net/news/egypt-sudan-smugglers-border-battles"> deploys military </a> to its southern border to stop smugglers from Sudan.
+`,
 `
 Iran is using Swedish criminal networks to target Jews, <a href="https://sakerhetspolisen.se/ovriga-sidor/other-languages/english-engelska/press-room/news/news/2024-05-30-iran-is-using-criminal-networks-in-sweden.html">Sweden's security service warns</a>. 
 The Iranian government had been using criminal networks within Sweden to carry out violent acts against other states, groups and individuals.
